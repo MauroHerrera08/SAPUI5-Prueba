4e891c8378cecb005df68ba5c99e8efbba8d600b
MauroHerrera08 <mauroherrera20@gmail.com>
2024-02-04 06:10:12 +0000

    First Commit

---
+++ employees/webapp/controller/App.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast, JSONModel) {
+        "use strict";
+
+        return Controller.extend("hcm.employees.controller.App", {
+            onInit: function () {
+                this._loadModel();
+            },
+
+/* Método privado con _ */
+            _loadModel: function (){
+                let oData = {
+                    employee: {
+                        name: 'Employee1'
+                    }
+                };
+                let oModel = new JSONModel(oData);
+                this.getView().setModel(oModel, "view") // Nombre con el que queda registrado el modelo "view"
+            },
+
+            onShowHello: function (){
+                MessageToast.show("Hello",{
+                    width: '10rem'
+                });
+            }
+        });
+    });
---
+++ employees/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["hcm/employees/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
